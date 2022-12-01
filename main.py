0eba9d2f602f2718f6284a889e4f456383078d09
HausCrafter8788 <101967716+HausCrafter8788@users.noreply.github.com>
2022-12-01 22:09:38 +0100

    change main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import nextcord
+import os
 
+from config import TOKEN
+from nextcord.ext import commands
+
+intents = nextcord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix=',', intents=intents)
+
+TOKEN = os.environ['TOKEN']
+
+cogs = []
+
+@bot.event
+async def on_ready():
+    print('Logget in as:')
+    print(str(bot.user.name) + ' ' + str(bot.user.id))
+    print("Loading Cogs . . .")
+    for cog in cogs:
+        cog_ = cog.split('.')
+        if cog_[2] == 'commands':
+            bot.load_extension(cog)
+            print('Command ' + cog_[3] + ' was loaded')
+        elif cog_[2] == 'events':
+            bot.load_extension(cog)
+            print('Event ' + cog_[3] + ' was loaded')
+        elif cog_[2] == 'tasks':
+            bot.load_extension(cog)
+            print('Task ' + cog_[3] + ' was loaded')
+
+bot.run(TOKEN)
